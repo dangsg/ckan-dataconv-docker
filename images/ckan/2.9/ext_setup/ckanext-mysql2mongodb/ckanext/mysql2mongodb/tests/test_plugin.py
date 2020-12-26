15e07198059dbf586ef1ca27ac6524d0fefdb314
dangsg <lyhoangdang96@gmail.com>
2020-12-27 00:46:21 +0700

    Remove unnecessary file and directory

--- images/ckan/2.9/ext_setup/ckanext-mysql2mongodb/ckanext/mysql2mongodb/tests/test_plugin.py
+++
@@ -1,5 +0,0 @@
-"""Tests for plugin.py."""
-import ckanext.mysql2mongodb.plugin as plugin
-
-def test_plugin():
-    pass
--- images/ckan/dangsg/ext_setup/run_setup.py
+++
@@ -1,3 +0,0 @@
-import os
-
-os.system("python /srv/app/src/ckanext-mysql2mongodb/setup.py develop")
