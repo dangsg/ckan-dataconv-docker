51a0a1cac2b136b4999d0253b1a9d610fbaea453
dangsg <lyhoangdang96@gmail.com>
2020-12-27 00:52:00 +0700

    Remove examples

--- setup.sh
+++
@@ -1,6 +0,0 @@
-cd compose
-docker-compose build
-docker-compose up
-docker-compose down
-docker cp ../setup/production.ini ckan:/srv/app/production.ini
-docker cp ../setup/ckanext-mysql2mongodb ckan:/srv/app/src/
